df1654e842181db2c797dce7dc42517e0d29b467
BikaashMeher <bm244368@gmail.com>
2024-12-07 00:58:19 +0530

    user model created

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors"
+import connectToDb from "./db/db.js";
+
+connectToDb();
 
 const app = express();
 dotenv.config();
